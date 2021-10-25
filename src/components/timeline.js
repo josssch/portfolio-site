23de4875abc6f6745d0b599f62e024e56d199ba3
srcjosh <birch.j@protonmail.com>
2021-10-25 16:09:50 +0100

    change: timeline simplified, now takes up space

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -20,29 +20,22 @@ export default class Timeline extends React.Component {
   }
 
   render() {
-    // the last year (being the latest in the timeline) subtracted by the first year (the oldest)
-    let latest   = this.years[this.years.length - 1];
-    let duration = latest - this.years[0];
-
-    // lambda function that calculates the percentage from the top based on the year
-    let distance = (year) => 100 - (latest - year) / duration * 100;
-
     return (
       <div className='timeline-wrapper'>
         <div className='timeline'>
           {
-            this.years.map(year =>
+            this.years.reverse().map(year =>
               <p className={ `timeline__year ${year === this.state.selectedYear ? 'selected' : ''}` } 
                   description={ this.times[year] } 
-                  style={ { top: `${ distance(year) }%` } }
                   onClick={evt => this.chooseYear(evt)}>
                     { year }
               </p>)
           }
-
         </div>
 
-        <p class='timeline__text' style={ { top: `${ distance(this.state.selectedYear) }%` } }>{ this.state.text }</p>
+        <div className='timeline__text-wrapper'>
+          <p className='timeline__text' style={ { top: `${ this.years.indexOf(this.state.selectedYear) / this.years.length * 100 }%` } }>{ this.state.text }</p>
+        </div>
       </div>
     );
   }
